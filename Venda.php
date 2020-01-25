4566ca911fdbb860a24f5bac893651ad3895b4ee
Raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-01-25 13:01:57 -0300

    tipagem

--- Venda.php
+++ Venda.php
@@ -1,30 +1,39 @@
 <?php
 
+ declare(strict_types=1);
 class Venda{
 
+
+/*
+@var string
+*/
 private $data;
 private $produto;
 private $quantidade;
 private $valorTotal;
 
-public function __construct($da,$pro,$quan,$val){
+public function __construct(String $da,String $pro,int $quan,float $val){
   $this->data = $da;
   $this->produto = $pro;
   $this->quantidade = $quan;
-  $this->$valorTotal = $val;
+
+  @$this->$valorTotal = $val;
+
 }
 
-public function getVenda(){
+public function getVenda():string{
   return
   "\nData da venda:".$this->data.
   "\nProduto da venda:".$this->produto.
   "\nQuantidade da venda:".$this->quantidade.
-  "\nValor total da venda:".$this->$valorTotal."\n";
+  "\nValor total da venda:".@$this->$valorTotal."\n";
 }
 
 }
 
-$Venda = new Venda("05/11/2001","Teste","1","100000");
+$Venda = new Venda("05/11/2001","Teste",1,100000);
 
 echo $Venda->getVenda();
+
+
  ?>
